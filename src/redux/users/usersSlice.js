4f7c825764c668897b70cc612c365f4276fe79e5
Braingenius2 <braingenius2@gmail.com>
2023-07-09 13:44:09 +0300

    Update usersSlice

--- src/redux/users/usersSlice.js
+++ src/redux/users/usersSlice.js
@@ -1,9 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+const initialState = {
+  users: [],
+  isLoading: false,
+  error: null,
+};
+
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: [],
+  extraReducers: {},
+
 });
 
 export default usersSlice.reducer;
